5575fe3a1d1c868193aad7bb86e3d3784f65c282
fabian.sucholas <fabian.sucholas@beone.pl>
2026-03-16 11:45:24 +0100

    error log check

--- .github/scripts/install-chrome-and-driver.sh
+++ .github/scripts/install-chrome-and-driver.sh
@@ -4,9 +4,7 @@ set -euo pipefail
 BASE_URL="https://googlechromelabs.github.io/chrome-for-testing"
 
 echo "➡️  Detecting current Stable-Version..."
-#VERSION=$(curl -s "$BASE_URL/last-known-good-versions.json" | jq -r '.channels.Stable.version')
-#VERSION="140.0.7339.207"
-VERSION="141.0.7390.54"
+VERSION=$(curl -s "$BASE_URL/last-known-good-versions.json" | jq -r '.channels.Stable.version')
 MILESTONE=$(echo "$VERSION" | cut -d. -f1)
 
 echo "📦 Stable Chrome Version: $VERSION (Milestone $MILESTONE)"
